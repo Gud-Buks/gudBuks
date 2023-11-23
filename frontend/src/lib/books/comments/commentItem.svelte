087d0539eaa3a82cc9b2a48e0da1992fe9fd496f
yolisses <iamurise@gmail.com>
2023-11-23 15:28:26 -0300

    fix delete comment route

--- frontend/src/lib/books/comments/commentItem.svelte
+++ frontend/src/lib/books/comments/commentItem.svelte
@@ -1,36 +1,34 @@
 <script lang="ts">
-  import type { Comment } from '$lib/types/comment';
-  import { timeAgo } from '../timeAgo';
-  import { api } from '$lib/api';
+	import { api } from '$lib/api';
+	import type { Comment } from '$lib/types/comment';
+	import { timeAgo } from '../timeAgo';
 
-  export let comment: Comment;
+	export let comment: Comment;
 
-  const date = new Date(comment.createdAt);
-  const id = comment.id;
+	const date = new Date(comment.createdAt);
+	const id = comment.id;
 
-  async function deleteComment() {
-    try {
-      await api.delete(`/comment/${id}`);
-	  console.log('Comentário excluído com sucesso!');
-    } catch (error) {
-      console.error('Erro ao excluir o comentário', error);
-    }
-  }
+	async function deleteComment() {
+		try {
+			await api.delete(`/comments/${id}`);
+			console.log('Comentário excluído com sucesso!');
+		} catch (error) {
+			console.error('Erro ao excluir o comentário', error);
+		}
+	}
 </script>
 
 <div>
-  <div>
-    {comment.text}
-  </div>
-  <div class="opacity-50 flex-row gap-2">
-    <div>
-      {comment?.user?.name}
-    </div>
-    <div>
-      {timeAgo(date)}
-    </div>
-    <button on:click={deleteComment}>
-      Excluir Comentário
-    </button>
-  </div>
+	<div>
+		{comment.text}
+	</div>
+	<div class="opacity-50 flex-row gap-2">
+		<div>
+			{comment?.user?.name}
+		</div>
+		<div>
+			{timeAgo(date)}
+		</div>
+		<button on:click={deleteComment}> Excluir Comentário </button>
+	</div>
 </div>
