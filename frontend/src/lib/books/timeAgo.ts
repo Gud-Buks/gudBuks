bb7385d9a9cdb42aad0c8c812c76006cc489c6c3
yolisses <iamurise@gmail.com>
2023-11-23 11:20:20 -0300

    use 'now' instead of 'x seconds ago'

--- frontend/src/lib/books/timeAgo.ts
+++ frontend/src/lib/books/timeAgo.ts
@@ -1,11 +1,12 @@
-export function timeAgo(fromDate: Date) {
-	const timeDifference = Math.floor((new Date().getTime() - fromDate.getTime()) / 1000);
-	const rtf = new Intl.RelativeTimeFormat(navigator.language, {
+export function timeAgo(fromDate: Date): string {
+	console.log(fromDate);
+	const timeDifference: number = Math.floor((new Date().getTime() - fromDate.getTime()) / 1000);
+	const rtf: Intl.RelativeTimeFormat = new Intl.RelativeTimeFormat(navigator.language, {
 		numeric: 'auto'
 	});
 
 	if (timeDifference < 60) {
-		return rtf.format(-timeDifference, 'second');
+		return rtf.format(0, 'second'); // "just now"
 	} else if (timeDifference < 3600) {
 		return rtf.format(-Math.floor(timeDifference / 60), 'minute');
 	} else if (timeDifference < 86400) {
