38484612f0fc0bd9eaaa7050102d800a00d5971d
yolisses <iamurise@gmail.com>
2023-11-23 12:23:52 -0300

    add signIn link in meLink

--- frontend/src/lib/user/meLink.svelte
+++ frontend/src/lib/user/meLink.svelte
@@ -2,10 +2,14 @@
 	import { me } from './meStore';
 </script>
 
-<a href="/me" class="bg-black/10 rounded-full w-10 aspect-square overflow-hidden">
-	<img
-		src={$me?.picture}
-		alt={$me?.name}
-		class="bg-black/10 rounded-full w-10 aspect-square overflow-hidden"
-	/>
-</a>
+{#if $me}
+	<a href="/me" class="bg-black/10 rounded-full w-10 aspect-square overflow-hidden">
+		<img
+			src={$me.picture}
+			alt={$me.name}
+			class="bg-black/10 rounded-full w-10 aspect-square overflow-hidden"
+		/>
+	</a>
+{:else}
+	<a class="main-button" href="/sign-in"> Sign In</a>
+{/if}
