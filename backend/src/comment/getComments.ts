a886112daaaa0acaf402552b2737f5fc23057b59
yolisses <iamurise@gmail.com>
2023-11-23 11:12:51 -0300

    include user in getComments

--- backend/src/comment/getComments.ts
+++ backend/src/comment/getComments.ts
@@ -1,5 +1,8 @@
 import { db } from "../db";
 
 export async function getComments(bookId: string) {
-  return db.comment.findMany({ where: { bookId } });
+  return db.comment.findMany({
+    where: { bookId },
+    include: { user: true },
+  });
 }
