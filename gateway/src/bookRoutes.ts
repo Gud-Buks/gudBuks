f91f88bae1caf2d42893c70c72d967392509d41e
yolisses <iamurise@gmail.com>
2023-11-23 10:44:12 -0300

    separate bookRoutes

---
+++ gateway/src/bookRoutes.ts
@@ -0,0 +1,44 @@
+import { Router } from "express";
+import { Book } from "./book";
+import { BooksVolume, BooksVolumes } from "./booksVolumes";
+
+const router = Router();
+export const bookRoutes = router;
+
+router.use("/books/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const booksRes = await fetch(
+    "https://www.googleapis.com/books/v1/volumes/" + id
+  );
+  const data: BooksVolume = await booksRes.json();
+  const book: Book = {
+    id: data.id,
+    title: data.volumeInfo.title,
+    authors: data.volumeInfo.authors,
+    description: data.volumeInfo.description,
+    cover: data.volumeInfo.imageLinks?.thumbnail,
+    publishedDate: data.volumeInfo.publishedDate,
+  };
+  return res.json(book);
+});
+
+router.use("/books", async (req, res) => {
+  const q = req.query.q as string;
+  if (!q) res.status(400).send("missing bookId query param");
+  const booksRes = await fetch(
+    "https://www.googleapis.com/books/v1/volumes?q=" + q
+  );
+  const booksData: BooksVolumes = await booksRes.json();
+  const books: Book[] = booksData.items.map((data) => {
+    return {
+      id: data.id,
+      title: data.volumeInfo.title,
+      authors: data.volumeInfo.authors,
+      description: data.volumeInfo.description,
+      cover: data.volumeInfo.imageLinks?.thumbnail,
+      publishedDate: data.volumeInfo.publishedDate,
+    };
+  });
+  return res.json(books);
+});
--- gateway/src/gateway.ts
+++ gateway/src/gateway.ts
@@ -2,53 +2,14 @@ import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import proxy from "express-http-proxy";
-import { Book } from "./book";
-import { BooksVolume, BooksVolumes } from "./booksVolumes";
+import { bookRoutes } from "./bookRoutes";
 import { errorMiddleware } from "./errorMiddleware";
 
 const app = express();
 const port = 8080;
 
 app.use(cors());
-
-app.use("/books/:id", async (req, res) => {
-  const id = req.params.id;
-
-  const booksRes = await fetch(
-    "https://www.googleapis.com/books/v1/volumes/" + id
-  );
-  const data: BooksVolume = await booksRes.json();
-  const book: Book = {
-    id: data.id,
-    title: data.volumeInfo.title,
-    authors: data.volumeInfo.authors,
-    description: data.volumeInfo.description,
-    cover: data.volumeInfo.imageLinks?.thumbnail,
-    publishedDate: data.volumeInfo.publishedDate,
-  };
-  return res.json(book);
-});
-
-app.use("/books", async (req, res) => {
-  const q = req.query.q as string;
-  if (!q) res.status(400).send("missing bookId query param");
-  const booksRes = await fetch(
-    "https://www.googleapis.com/books/v1/volumes?q=" + q
-  );
-  const booksData: BooksVolumes = await booksRes.json();
-  const books: Book[] = booksData.items.map((data) => {
-    return {
-      id: data.id,
-      title: data.volumeInfo.title,
-      authors: data.volumeInfo.authors,
-      description: data.volumeInfo.description,
-      cover: data.volumeInfo.imageLinks?.thumbnail,
-      publishedDate: data.volumeInfo.publishedDate,
-    };
-  });
-  return res.json(books);
-});
-
+app.use("/api/v1", bookRoutes);
 app.use("/", proxy("http://localhost:3000"));
 
 app.use(errorMiddleware);
