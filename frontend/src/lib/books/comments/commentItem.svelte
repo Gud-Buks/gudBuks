52927d1d577ac52835f007bdd21970217907b3c7
João Alfredo Alves <68473607+JoaoAlfredoAlves@users.noreply.github.com>
2023-11-23 14:56:20 -0300

    creating deleteCommentButton

--- frontend/src/lib/books/comments/commentItem.svelte
+++ frontend/src/lib/books/comments/commentItem.svelte
@@ -1,22 +1,27 @@
 <script lang="ts">
-	import type { Comment } from '$lib/types/comment';
-	import { timeAgo } from '../timeAgo';
+  import type { Comment } from '$lib/types/comment';
+  import { timeAgo } from '../timeAgo';
+  import { api } from '$lib/api';
 
-	export let comment: Comment;
+  export let comment: Comment;
+	
+  const date = new Date(comment.createdAt);
 
-	const date = new Date(comment.createdAt);
 </script>
 
 <div>
-	<div>
-		{comment.text}
-	</div>
-	<div class="opacity-50 flex-row gap-2">
-		<div>
-			{comment?.user?.name}
-		</div>
-		<div>
-			{timeAgo(date)}
-		</div>
-	</div>
+  <div>
+    {comment.text}
+  </div>
+  <div class="opacity-50 flex-row gap-2">
+    <div>
+      {comment?.user?.name}
+    </div>
+    <div>
+      {timeAgo(date)}
+    </div>
+    <button on:click={api.delete('/comment/'+id)}>
+      Excluir Comentário
+    </button>
+  </div>
 </div>
